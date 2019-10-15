df5f605a2213804d66a3c7a29d8f38bd2dcb02ad
Golden Dragon <zou_406143883@qq.com>
2019-10-16 00:20:13 +0800

    [refract] extract services out

--- routes.py
+++ routes.py
@@ -1,29 +1,7 @@
-import json
-from views import *
-from db import dbService
-from platforms import PraserService
-from core import Extractor, Downloader
-
-def register_services():
-
-    with open('./config.json', 'r') as f:
-        cfg = json.loads(f.read())
-        
-        platforms_cfg = cfg['platforms']
-        dbpath = cfg['database']['path']
-        ffmpegpath = cfg['core-extract']["ffmpeg-path"]
-        mediafolder = cfg['mediafolder']["path"]
-
-        parser = PraserService(platforms_cfg)
-        extractor = Extractor(ffmpegpath, mediafolder)
-        dolder = Downloader(mediafolder)
-        localdb = dbService(dbpath)
-
-        return (
-            Cores([extractor, dolder]),
-            Users([localdb]),
-            Platforms([parser])
-        )
+'''
+    setup routes
+'''
+from services import register_services
 
 
 def setup_routes(app):
